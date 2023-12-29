9f65cc9fec559cc8b7919a82f3e098c6df0decfe
JamesDimasayao <148727571+JamesDimasayao@users.noreply.github.com>
2023-12-29 17:32:34 +0800

    Add files via upload

---
+++ James_Poertfolio/home.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Digital Artist Portfolio</title>
+    <link rel="stylesheet" href="/style/style.css">
+</head>
+<body>
+    <header>
+        <h1>JAMES  - Traditional Artist</h1>
+        <nav>
+            <ul>
+                <li><a href="#about">About</a></li>
+                <li><a href="#portfolio">Portfolio</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <section id="about">
+        <h2>About Me</h2>
+        <p>I like to draw, but I'm not serious about pursuing it until college, so here we are. During my first year, my drawing skills improved as the days went by, and I was proud of the results.</p>
+    </section>
+
+    <section id="portfolio">
+        <h2>Portfolio</h2>
+        <!-- Add your artwork here with image and description -->
+        <div class="artwork">
+            <img src="img/Img_1" alt="Artwork 1">
+            <p>Description of Artwork 1.</p>
+        </div>
+        <div class="artwork">
+            <img src="artwork2.jpg" alt="Artwork 2">
+            <p>Description of Artwork 2.</p>
+        </div>
+        <!-- Add more artworks as needed -->
+    </section>
+
+    <section id="contact">
+        <h2>Contact</h2>
+        <p>Feel free to reach out to me through the following channels:</p>
+        <ul>
+            <li>Email: James@gmail.com</li>
+            <li>Instagram: @James</li>
+            <li>Twitter: @your_twitter</li>
+        </ul>
+    </section>
+
+    <footer>
+        <p>&copy; 2023 Your Name - Digital Artist</p>
+    </footer>
+</body>
+</html>
